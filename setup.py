be5b5dc0895a3dfa186bdf2c0a6638de6559203f
Alan <alanhaugen@gmail.com>
2020-04-04 23:42:42 +0200

    Changed name of source directory to trainer
    Trainer directory now has the deep learning model and training pipeline

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-        name='source',
+        name='trainer',
         version='1.0',
-        description='This is made for a kaggle pytorch competition. I have never written a standard python project before',
+        description='Train a kenyan food dataset with a classification model with PyTorch',
         author='Alan Haugen',
         author_email='alanhaugen@gmail.com',
-        packages=['source'],  #same as name
+        packages=['trainer'],  #same as name
         install_requires=['pandas', 'numpy', 'pytorch', 'torchvision', 'tensorboard', 'kaggle'], #external packages as dependencies
 )
